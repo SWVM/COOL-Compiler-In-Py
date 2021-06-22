bd4d8ece1387b53f8f30720c929d4aea664e71e7
SWVM <lmax.qaz@gmail.com>
2021-06-21 22:33:10 -0500

    Getting started

---
+++ Interpreter/interpreter.py
@@ -0,0 +1,23 @@
+
+
+class Env():
+    def __init__(self):
+        self.e = {}
+        self.s = {}
+
+    def epop(self, k):
+
+    def spop(self, k):
+
+    def eadd(self, k, v):
+
+    def sadd(self, k, v):
+
+
+
+class Cool_Prog():
+
+class Cool_Expr():
+    def __init__(self):
+        pass
+    def read_file():
