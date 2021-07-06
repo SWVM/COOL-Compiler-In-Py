09326c3de4dcaf7e108180e77a2fe0d052c5da79
SWVM <lmax.qaz@gmail.com>
2021-07-05 23:18:49 -0500

    Argument parsing
    With argparse

---
+++ Cool.py
@@ -0,0 +1,21 @@
+import argparse
+
+arg_parser = argparse.ArgumentParser(description="Cool Interpretor")
+arg_parser.add_argument("input_file", type=str, nargs=1,
+                        help="path of the input file")
+arg_parser.add_argument("--lex", action="store_true",
+                        help="emit cl-lex file(tokenized input)")
+arg_parser.add_argument("--parse", action="store_true",
+                        help="emit cl-ast file(cool abstract syntax tree)")
+arg_parser.add_argument("--type", action="store_true",
+                        help="emit cl-type file(type checked ast)")
+arg_parser.add_argument("--class-map", action="store_true",
+                        help="emit cl-type file(classes & attributes)")
+arg_parser.add_argument("--imp-map", action="store_true",
+                        help="emit cl-type file(classes & methods)")
+arg_parser.add_argument("--parent-map", action="store_true",
+                        help="emit cl-type file(classes & inheritance)")
+
+
+print(arg_parser.parse_args( ["--lex"] ))
+
